1c4eb790bd22bf508714e73341aa03894c9009b7
Max <oscar.torres.m@uni.pe>
2026-05-31 22:47:52 -0500

    WIP physics 3D structure

--- src/public/Core/Physics/Physics3D/Octree.h
+++ src/public/Core/Physics/Physics3D/Octree.h
@@ -1,5 +1,79 @@
 #pragma once
+#include <glm/glm.hpp>
+#include <memory>
+#include <vector>
+
+namespace Physics3D {
+
+class PhysicObject;
 
 class Octree {
+public:
+	static constexpr int MAX_OBJECTS = 4;
+	static constexpr int MAX_DEPTH = 8;
 
+private:
+	struct Node {
+		glm::vec3 bottomLeft;
+		glm::vec3 topRight;
+		int depth;
+
+		std::vector<PhysicObject*> objects;
+		std::unique_ptr<Node> children[4];
+		bool isSplit = false;
+
+		/// @brief Gets the world center of the node.
+		/// @return The center  of the node.
+		glm::vec3 Center() const;
+
+		/// @brief Checks if the node intersect with a given physic object.
+		/// @param obj The object to check.
+		/// @return True if the node intersects.
+		bool Overlap(PhysicObject* obj) const;
+
+		bool Contains(PhysicObject* obj) const;
+	};
+
+	std::unique_ptr<Node> root;
+
+	/// @brief
+	///		Inserts an object into a given node if the node contains the object.
+	/// 	If no, do nothing.
+	/// @param node The node where to insert.
+	/// @param obj 	The object to insert.
+	void Insert(Node* node, PhysicObject* obj);
+
+	/// @brief Splits the a given node.
+	/// @param node The node to split.
+	void Split(Node* node);
+
+	/// @brief Auxiliar function of GetNearObjects to recursive searchs in the tree.
+	/// @param node the Node to analyze.
+	/// @param object The object of GetNearObjects.
+	/// @param out The out vector that contains all of the candidates.
+	void QueryNode(
+		const Node* node,
+		PhysicObject* object,
+		std::vector<PhysicObject*>& out) const;
+
+public:
+	/// @brief Builds the quadtree using an object list withing certain limits
+	/// @param objects The reference of the objects.
+	/// @param worldMin The bottom left point of the tree.
+	/// @param worldMax Te  top right point of the tree.
+	void Build(
+		const std::vector<PhysicObject*>& objects,
+		glm::vec3 worldMin,
+		glm::vec3 worldMax);
+
+	/// @brief Gets the near objects arround a given object.
+	/// @param object The object to check arround.
+	/// @param out A vector of the objects arround the given object.
+	void GetNearObjects(
+		PhysicObject* object,
+		std::vector<PhysicObject*>& out) const;
+
+	/// @brief Clears the tree.
+	void Clear();
 };
+}
--- src/public/Core/Physics/Physics3D/PhysicsEngine.h
+++ src/public/Core/Physics/Physics3D/PhysicsEngine.h
@@ -1,25 +1,54 @@
 #pragma once
-#include <vector>
-#include <glm/glm.hpp>
 #include <Core/Physics/APhysicsEngine.h>
+#include <glm/glm.hpp>
+#include <vector>
 
 class AObject;
+class ACollider3DComponent;
+class Rigidbody3DComponent;
 
 namespace Physics3D {
-	/**
-	 * This class encasulates all the physics calculations for 3D objects.
-	 */
-	class PhysicsEngine : public APhysicsEngine {
 
-	public:
-		/**
-		 * This class represents a 3D raycast hit.
-		 */
-		class Raycast3D {
-		public:
-			bool hit = false;
-			float time;
-			glm::vec3 position;
-		};
+class PhysicObject;
+class Octree;
+/**
+ * This class encasulates all the physics calculations for 3D objects.
+ */
+class PhysicsEngine : public APhysicsEngine {
+
+private:
+	struct CollisionManifold3D{
+		glm::vec3 PenetrationVector;
+		float Depth;
 	};
+
+	std::vector<PhysicObject*> physicObjects;
+
+	Octree* octree;
+	glm::vec3 worldMin = { -500, -500, -500 };
+	glm::vec3 worldMax = { 500, 500, 500 };
+
+public:
+	PhysicsEngine();
+
+protected:
+	void RegisterObject(AObject* object) override;
+
+	void MoveObjects() override;
+	void MoveRigidbody(Rigidbody3DComponent* rigidbody);
+	void UpdateAceleration(Rigidbody3DComponent* rigidbody);
+	void UpdateVelocity(Rigidbody3DComponent* rigidbody);
+	void UpdatePosition(Rigidbody3DComponent* rigidbody);
+
+	void UpdateTree() override;
+	void UpdateContainers();
+
+	void CheckCollisions() override;
+	bool CheckCollision(PhysicObject* physicObjectA, PhysicObject* physicObjectB);
+	void ResolveCollision2D(PhysicObject* physicObjectA, PhysicObject* physicObjectB, CollisionManifold manifold);
+
+private:
+	void Resolve2RBCollision(PhysicObject* physicObjectA, PhysicObject* physicObjectB, CollisionManifold manifold);
+	void Resolve1RBCollision(PhysicObject* physicObject, CollisionManifold manifold);
+};
 }
