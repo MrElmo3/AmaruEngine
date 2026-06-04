31b8587bc9a6aa69f0573ba60212dcc4f49c729d
Oscar Torres <oscar.torres@gdpteam.com>
2026-06-04 11:24:03 -0500

    Feat(SphereColliderComponent): Implemented Sphere collider

---
+++ src/private/Core/Components/Physics/3D/SphereColliderComponent.cpp
@@ -0,0 +1,48 @@
+#include <Core/Components/Physics/3D/SphereColliderComponent.h>
+
+SphereColliderComponent::SphereColliderComponent(AObject* parent, glm::vec3 position, float radius)
+	: ACollider3DComponent(parent, position) {
+	SetRadius(radius);
+}
+
+SphereColliderComponent::SphereColliderComponent(AObject* parent, glm::vec3 position)
+	:  SphereColliderComponent(parent, position, 0.5f) {}
+
+SphereColliderComponent::SphereColliderComponent(AObject* parent)
+	: SphereColliderComponent(parent, glm::vec3(0)) {
+}
+
+SphereColliderComponent::~SphereColliderComponent() {
+	SetRadius(0.f);
+	ACollider3DComponent::~ACollider3DComponent();
+}
+
+void SphereColliderComponent::UpdateLocalVertexPoints() {
+	vertexPoints.clear();
+	vertexPoints.reserve(RENDER_VERTEX_NUMBER * 3);
+
+	float angleStep = 360.0f / RENDER_VERTEX_NUMBER;
+
+	for (int i = 0; i < RENDER_VERTEX_NUMBER; i++) {
+		float angleInRadians = glm::radians(angleStep * i);
+
+		float cosA = glm::cos(angleInRadians);
+		float sinA = glm::sin(angleInRadians);
+
+		vertexPoints.push_back(glm::vec3(radius * cosA, radius * sinA, 0.0f));
+		vertexPoints.push_back(glm::vec3(radius * cosA, 0.0f, radius * sinA));
+		vertexPoints.push_back(glm::vec3(0.0f, radius * cosA, radius * sinA));
+	}
+}
+
+glm::vec3 SphereColliderComponent::GetSupportPoint(glm::vec3 direction) {
+	if(glm::length(direction) < 0.0001f) return ACollider::GetSupportPoint(direction);
+
+	return center + glm::normalize(direction) * radius;
+}
+
+SphereColliderComponent* SphereColliderComponent::SetRadius(float radius) {
+	this->radius = radius;
+	UpdateLocalVertexPoints();
+	return this;
+}
